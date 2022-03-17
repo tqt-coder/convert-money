cdf75b68e08c05a2a3c4c21497c92f4222337c1b
tranquoctuan2001 <76949884+tranquoctuan2001@users.noreply.github.com>
2022-03-17 20:35:41 +0700

    save file

---
+++ We/Controllers/ChangeMoneyController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace We.Controllers
+{
+    public class ChangeMoneyController : Controller
+    {
+        // GET: ChangeMoney
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ChangeMoney/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ChangeMoney/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ChangeMoney/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            using (ServiceReference1.WebService1SoapClient ws = new ServiceReference1.WebService1SoapClient())
+            {
+
+                try
+                {
+                    string number = form["inputMoney"].ToString();
+                    string typeMoney = form["typeMoney"].ToString();
+
+                    if (number != null && typeMoney != null)
+                    {
+                        double tempt = Convert.ToDouble(number);
+                        double result = 0;
+                        switch (typeMoney)
+                        {
+                            case "VND2USD":
+                                result = ws.VNDToUSD(tempt);
+                                break;
+                            case "VND2EUR":
+                                result = ws.VNDToEUR(tempt);
+                                break;
+                            case "USD2VND":
+                                result = ws.USDToVND(tempt);
+                                break;
+                            case "EUR2VND":
+                                result = ws.EURToVND(tempt);
+                                break;
+                        }
+                        TempData["number"] = tempt;
+                        TempData["result"] = Math.Round(result, 3);
+                    }
+
+                    return RedirectToAction("Index", "Home");
+                }
+                catch
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+
+
+
+    }
+}
---
+++ We/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace We.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (TempData["result"] != null && TempData["number"] != null)
+            {
+                ViewBag.Result = TempData["result"].ToString();
+                ViewBag.Number = TempData["number"].ToString();
+            }
+            return View();
+        }
+
+       
+    }
+}
