0fd3c1afa9d64f987f49936cb3ec4b3f1e207beb
tranquoctuan2001 <76949884+tranquoctuan2001@users.noreply.github.com>
2022-03-17 11:43:46 +0700

    add references webservice

---
+++ ClientConvertWebService/Form1.cs
@@ -0,0 +1,10 @@
+namespace ClientConvertWebService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private ServiceReference1.WebService1SoapClient ws;
+        public Form1()
+        {
+            InitializeComponent();
+            ws = new ServiceReference1.WebService1SoapClient();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                this.txt1.Text = ws.USDToVND(10).ToString();
+            
+        }
+    }
+}
